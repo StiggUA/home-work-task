df6d3b92c6a1605718d0d4a950465a7295de6b03
Serhii Stupin <sergey.stupin1987@gmail.com>
2019-11-01 20:54:51 +0200

    New changes

--- src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
+++ src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
@@ -3,18 +3,27 @@ package com.hillel.lecture_4;
 /**
  * Created by alpa on 10/25/19
  */
+/**
+ * Написать программу, которая перевод десятичные числа в двоичные и наоборот.
+ */
 public class BinaryToDecimalChecker {
 
     public int fromDecimalToBinary(int value) {
 
 //        TODO implements result
         int result = 0;
+        int i = 0;
+        for (i=0; i<200; i++)
+        {
+            value = value/2;
+            value = value%2;
+            result = value%2;
+            if (value == 0) break;
+        }
 
         return result;
     }
-
     public int fromBinaryToDecimal(int value) {
-
 //        TODO implements result
         int result = 0;
 
