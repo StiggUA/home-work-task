7a6ca25ababf4df47b843fe48b6526e737acfec2
Serhii Stupin <sergey.stupin1987@gmail.com>
2019-11-07 17:05:27 +0200

    SquareW0rd

--- src/main/java/com/hillel/lecture_6/SquareWordPatternChecker.java
+++ src/main/java/com/hillel/lecture_6/SquareWordPatternChecker.java
@@ -12,33 +12,40 @@ public class SquareWordPatternChecker {
     public String applySquareWordPattern(String word) {
 
 //        TODO implements result
-        String result = "";
-        int len = word.length();
+        String result = word;
 
-        // Generate all rotations one by one and print
-        StringBuffer sb;
+        String res = word;
 
-        for (int i = 0; i < len; i++) {
-            sb = new StringBuffer();
-
-            int j = i;  // Current index in str
-            int k = 0;  // Current index in temp
-
-            for (int k2 = j; k2 < word.length(); k2++) {
-                sb.insert(k, word.charAt(j));
-                k++;
-                j++;
-            }
-
-            j = 0;
-            while (j < i) {
-                sb.insert(k, word.charAt(j));
-                j++;
-                k++;
-            }
-            result = word;
+        for (int i = 1; i < word.length(); i++) {
+            res = res.substring(1) + res.charAt(0);
+            result = result + ", " + res;
         }
         return result;
     }
 
 }
+
+
+//        int len = word.length();
+//
+//        // Generate all rotations one by one and print
+//        StringBuffer sb;
+//
+//        for (int i = 0; i < len; i++) {
+//        sb = new StringBuffer();
+//
+//        int j = i;  // Current index in str
+//        int k = 0;  // Current index in temp
+//
+//        for (int k2 = j; k2 < word.length(); k2++) {
+//        sb.insert(k, word.charAt(j));
+//        k++;
+//        j++;
+//        }
+//
+//        j = 0;
+//        while (j < i) {
+//        sb.insert(k, word.charAt(j));
+//        j++;
+//        k++;
+//        }
