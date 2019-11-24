378a78d6713f9dfa792a24c7023359ea45158e4c
Serhii Stupin <sergey.stupin1987@gmail.com>
2019-11-24 16:05:44 +0200

    fix

--- src/main/java/com/hillel/Lecture_10/Calculator/Programming.java
+++ src/main/java/com/hillel/Lecture_10/Calculator/Programming.java
@@ -2,9 +2,17 @@ package com.hillel.Lecture_10.Calculator;
 
 public class Programming extends AbstractCalculator {
 
-    public void cycleCount(double a){
-        for (int i = 0; i < a; i++) {
-            System.out.println(String.format("%s -> This program works " + (a - i) + " times",  this.getClass().getSimpleName()));
+    public void cycleCount(int a) {
+        for (int i = 0; i <= a; i++) {
+            if (i == 0) {
+                System.out.println(String.format("%s -> The final countdown", this.getClass().getSimpleName()));
+            }
+            else if(i == a){
+                System.out.println(String.format("%s -> Gooooooooooooo", this.getClass().getSimpleName()));
+            }
+                else {
+                System.out.println(String.format("%s -> " + (a - i) + "" , this.getClass().getSimpleName()));
+            }
         }
     }
     public void random(int a) {
