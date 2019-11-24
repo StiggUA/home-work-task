bba74d349140bfe6a966426cf8bc5b4366f28e04
Serhii Stupin <sergey.stupin1987@gmail.com>
2019-11-24 15:49:32 +0200

    fix

--- src/main/java/com/hillel/Lecture_10/Calculator/AbstractCalculator.java
+++ src/main/java/com/hillel/Lecture_10/Calculator/AbstractCalculator.java
@@ -2,9 +2,9 @@ package com.hillel.Lecture_10.Calculator;
 
 public abstract class AbstractCalculator {
 
-    public void plus(double a, double b) {System.out.println(a + b);}
-    public void minus(double a, double b) {System.out.println(a - b); }
-    public void multiply(double a, double b) {System.out.println(a * b);}
-    public void divide(double a, double b) {System.out.println(a / b);}
+    public void plus(double a, double b) {System.out.println(String.format("%s -> " + (a + b) + "",  this.getClass().getSimpleName()));}
+    public void minus(double a, double b) {System.out.println(String.format("%s -> " + (a - b) + "",  this.getClass().getSimpleName()));}
+    public void multiply(double a, double b) {System.out.println(String.format("%s -> " + (a * b) + "",  this.getClass().getSimpleName()));}
+    public void divide(double a, double b) {System.out.println(String.format("%s -> " + (a /     b) + "",  this.getClass().getSimpleName()));}
 
 }
--- src/main/java/com/hillel/Lecture_10/Calculator/UsualCalc.java
+++ src/main/java/com/hillel/Lecture_10/Calculator/UsualCalc.java
@@ -2,11 +2,11 @@ package com.hillel.Lecture_10.Calculator;
 
 public class UsualCalc extends AbstractCalculator{
     @Override
-    public void plus(double a, double b) {System.out.println(a + b);}
+    public void plus(double a, double b) {System.out.println(String.format("%s -> " + (a + b) + "",  this.getClass().getSimpleName()));}
     @Override
-    public void minus(double a, double b) {System.out.println(a - b); }
+    public void minus(double a, double b) {System.out.println(String.format("%s -> " + (a - b) + "",  this.getClass().getSimpleName()));}
     @Override
-    public void multiply(double a, double b) {System.out.println(a * b);}
+    public void multiply(double a, double b) {System.out.println(String.format("%s -> " + (a * b) + "",  this.getClass().getSimpleName()));}
     @Override
-    public void divide(double a, double b) {System.out.println(a / b);}
+    public void divide(double a, double b) {System.out.println(String.format("%s -> " + (a / b) + "",  this.getClass().getSimpleName()));}
 }
