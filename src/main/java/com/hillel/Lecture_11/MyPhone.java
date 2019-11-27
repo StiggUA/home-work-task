45d960a21a31fbb61522a73d1ba982a49a922baa
StiggUA <sergey.stupin1987@gmail.com>
2019-11-27 09:04:22 +0200

    Telegram main

---
+++ src/main/java/com/hillel/Lecture_11/MyPhone.java
@@ -0,0 +1,27 @@
+package com.hillel.Lecture_11;
+
+public class MyPhone {
+    public static void main(String[] args) {
+        Telegram telegram = new Telegram();
+        telegram.messageReceive();
+        telegram.sendTextMessage();
+        telegram.audioCall();
+        telegram.builtInBrowser();
+        telegram.chatWithYourself();;
+        telegram.contactImport();
+        telegram.addPeopleNearby();
+        telegram.createChannel();
+        telegram.groupChat();
+        telegram.messageSearch();
+        telegram.messageShare();
+        telegram.notifications();
+        telegram.privateChat();
+        telegram.profileSetting();
+        telegram.sendFile();
+        telegram.sendPicture();
+        telegram.sendSticker();
+        telegram.sendVideo();
+        telegram.takePhoto();
+        telegram.takeVideo();
+    }
+}
