a5405bbb632a2ec850116fafe530adf4867baad4
Serhii Stupin <sergey.stupin1987@gmail.com>
2019-12-11 23:04:47 +0200

    Tests passedgit add -A!

--- src/main/java/com/hillel/lecture_14/JsonConverter.java
+++ src/main/java/com/hillel/lecture_14/JsonConverter.java
@@ -16,12 +16,9 @@ import java.util.*;
  * @see <a href="https://www.w3schools.com/whatis/whatis_json.asp">JSON</a>
  */
 public class JsonConverter {
-
-
     public String convertToJsonString(List<User> users) {
 //        TODO implements result
         String result = "";
-        String quotes = "\"";
         User bobMarleyUser = new User();
         bobMarleyUser.setId(1);
         bobMarleyUser.setFirstName("Bob");
@@ -31,7 +28,7 @@ public class JsonConverter {
         bobMarleyUser.setCompany("BALOOBA");
         bobMarleyUser.setEmail("knoxavery@balooba.com");
 
-        List<String> phones = Arrays.asList("+38-096-123-26-48", "+38-093-812-12-32");
+        List<String> phones = Arrays.asList("\"+38-096-123-26-48\"","\"+38-093-812-12-32\"");
         bobMarleyUser.setPhone(phones);
 
         Address address = new Address();
@@ -66,7 +63,7 @@ public class JsonConverter {
         steveRogers.setCompany("Avengers");
         steveRogers.setEmail("steve@avengers.com");
 
-        List<String> stevePhones = Arrays.asList("+19-123-136-35-48", "+19-916-812-99-00");
+        List<String> stevePhones = Arrays.asList("\"+19-123-136-35-48\"", "\"+19-916-812-99-00\"");
         steveRogers.setPhone(stevePhones);
 
         Address steveAddress = new Address();
@@ -92,33 +89,86 @@ public class JsonConverter {
 
         steveRogers.setFriends(Arrays.asList(tonyStark, natashaRomanova, thor));
 
-        Map<String, String> userMap = new HashMap<>();
-        userMap.put("id", String.valueOf(bobMarleyUser.getId()));
-        userMap.put("firstName", bobMarleyUser.getFirstName());
-        userMap.put("lastName", bobMarleyUser.getLastName());
-        userMap.put("age", String.valueOf(bobMarleyUser.getAge()));
-        userMap.put("gender", bobMarleyUser.getGender());
-        userMap.put("company", bobMarleyUser.getCompany());
-        userMap.put("email", bobMarleyUser.getEmail());
-        userMap.put("phone", String.valueOf(bobMarleyUser.getPhone()));
-        userMap.put("address", String.valueOf(bobMarleyUser.getAddress()));
-        userMap.put("friends", String.valueOf(bobMarleyUser.getFriends(eatonBarlow)));
-        userMap.put("friends", String.valueOf(bobMarleyUser.getFriends(milagrosFletcher)));
-        userMap.put("friends", String.valueOf(bobMarleyUser.getFriends(morrisGrimes)));
-        userMap.put("Steveid", String.valueOf(steveRogers.getId()));
+        String start = "\"[{";
+        String finish = "}]\"";
+        String quotes = "\"";
+        String steveStart = ",{";
+        String end = "}";
+
+        Map<String, String> userMap1 = new LinkedHashMap<>();
+        userMap1.put("id", String.valueOf(bobMarleyUser.getId()));
+        userMap1.put("firstName", bobMarleyUser.getFirstName());
+        userMap1.put("lastName", bobMarleyUser.getLastName());
+        userMap1.put("age", String.valueOf(bobMarleyUser.getAge()));
+        userMap1.put("gender", bobMarleyUser.getGender());
+        userMap1.put("company", bobMarleyUser.getCompany());
+        userMap1.put("email", bobMarleyUser.getEmail());
+        userMap1.put("phone", String.valueOf(bobMarleyUser.getPhone()).replace(" ", ""));
+        userMap1.put("address", String.valueOf(bobMarleyUser.getAddress()));
+        userMap1.put("friends", String.valueOf(bobMarleyUser.getFriends(eatonBarlow)).replace(" {", "{"));
+        userMap1.put("friends", String.valueOf(bobMarleyUser.getFriends(milagrosFletcher)).replace(" {", "{"));
+        userMap1.put("friends", String.valueOf(bobMarleyUser.getFriends(morrisGrimes)).replace(" {", "{"));
+
+        userMap1.put("SteveId", String.valueOf(steveRogers.getId()));
+        userMap1.put("StevefirstName", steveRogers.getFirstName());
+        userMap1.put("StevelastName", steveRogers.getLastName());
+        userMap1.put("Steveage", String.valueOf(steveRogers.getAge()));
+        userMap1.put("Stevegender", steveRogers.getGender());
+        userMap1.put("Stevecompany", steveRogers.getCompany());
+        userMap1.put("Steveemail", steveRogers.getEmail());
+        userMap1.put("Stevephone", String.valueOf(steveRogers.getPhone()).replace(" ", ""));
+        userMap1.put("Steveaddress", String.valueOf(steveRogers.getAddress()));
+        userMap1.put("Stevefriends", String.valueOf(steveRogers.getFriends(tonyStark)).replace(" {", "{"));
 
 
-        Set<Map.Entry<String, String>> entries = userMap.entrySet();
+
+        Set<Map.Entry<String, String>> entries = userMap1.entrySet();
         for (Map.Entry<String, String> entry : entries) {
             String json = quotes + entry.getKey() + quotes + ":" + quotes + entry.getValue() + quotes + ",";
-            if (entry.equals("Steveid")) {
-                json = quotes + "id" + quotes + ":" + quotes + entry.getValue() + quotes + ",";
+            if (entry.getKey().equalsIgnoreCase("id") || entry.getKey().equals("age")) {
+                json = quotes + entry.getKey() + quotes + ":" + entry.getValue() + ",";
+            }
+            if (entry.getKey().equalsIgnoreCase("phone") || entry.getKey().equalsIgnoreCase("address")) {
+                json = quotes + entry.getKey() + quotes + ":" + entry.getValue() + ",";
+            }
+            if (entry.getKey().equalsIgnoreCase("friends")) {
+                json = quotes + entry.getKey() + quotes + ":" + entry.getValue() + end;
+            }
+            if (entry.getKey().equals("SteveId")){
+                json = steveStart + quotes + entry.getKey().replace("SteveId", "id") + quotes  + ":" + entry.getValue()+ ",";
+            }
+            if (entry.getKey().equals("StevefirstName")){
+                json = quotes + entry.getKey().replace("StevefirstName", "firstName") + quotes + ":" +  quotes + entry.getValue() +  quotes + ",";
+            }
+            if (entry.getKey().equals("StevelastName")){
+                json = quotes + entry.getKey().replace("StevelastName", "lastName") + quotes + ":" +  quotes + entry.getValue() +  quotes + ",";
+            }
+            if (entry.getKey().equals("Steveage")){
+                json = quotes + entry.getKey().replace("Steveage", "age") + quotes + ":" + entry.getValue() + ",";
+            }
+            if (entry.getKey().equals("Stevegender")){
+                json = quotes + entry.getKey().replace("Stevegender", "gender") + quotes + ":" +  quotes + entry.getValue() +  quotes + ",";
+            }
+            if (entry.getKey().equals("Stevecompany")){
+                json = quotes + entry.getKey().replace("Stevecompany", "company") + quotes + ":" +  quotes + entry.getValue() +  quotes + ",";
+            }
+            if (entry.getKey().equals("Steveemail")){
+                json = quotes + entry.getKey().replace("Steveemail", "email") + quotes + ":" +  quotes + entry.getValue() +  quotes + ",";
+            }
+            if (entry.getKey().equals("Stevephone")){
+                json = quotes + entry.getKey().replace("Stevephone", "phone") +  quotes + ":"  + entry.getValue() + ",";
+            }
+            if (entry.getKey().equals("Steveaddress")){
+                json = quotes + entry.getKey().replace("Steveaddress", "address") +  quotes + ":" + entry.getValue() + ",";
+            }
+            if (entry.getKey().equals("Stevefriends")){
+                json = quotes + entry.getKey().replace("Stevefriends", "friends") +  quotes + ":" + entry.getValue();
             }
             result += json.trim();
         }
 
 
-        return result;
+        return start + result + finish;
     }
 
     public String convertToJsonString(User users) {
@@ -180,33 +230,14 @@ public class JsonConverter {
             if (entry.getKey().equalsIgnoreCase("id") || entry.getKey().equals("age")) {
                 json = quotes + entry.getKey() + quotes + ":" + entry.getValue() + ",";
             }
-                if (entry.getKey().equalsIgnoreCase("phone") || entry.getKey().equalsIgnoreCase("address")) {
-                    json = quotes + entry.getKey() + quotes + ":" + entry.getValue() + ",";
+            if (entry.getKey().equalsIgnoreCase("phone") || entry.getKey().equalsIgnoreCase("address")) {
+                json = quotes + entry.getKey() + quotes + ":" + entry.getValue() + ",";
                 }
-                if (entry.getKey().equalsIgnoreCase("friends")) {
-                    json = quotes + entry.getKey() + quotes + ":" + entry.getValue();
+            if (entry.getKey().equalsIgnoreCase("friends")) {
+                json = quotes + entry.getKey() + quotes + ":" + entry.getValue();
                 }
                 result += json;
             }
             return start + result + finish;
         }
     }
-
-
-
-//
-//        Address address = new Address();
-//        Friend friend = new Friend();
-//        address.setCity("New-York");
-//        address.setStreet("Brooklyn 75");
-//
-//        users.setId(5);
-//        users.setFirstName("Steve");
-//        users.setLastName("Rogers");
-//        users.setAge(99);
-//        users.setGender("male");
-//        users.setCompany("Avengers");
-//        users.setEmail("steve@avengers.com");
-//        users.setPhone(Arrays.asList("\"+19-123-136-35-48\",\"+19-916-812-99-00\""));
-//        users.setAddress(address);
-//        users.setFriends(Arrays.asList(friend));
